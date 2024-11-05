26f395106e8a9391332c998f785c4468664a49aa
Marek <marek@culak.sk>
2024-11-05 11:00:12 +0000

    final

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,2 +1,2 @@
-@props(['width' => 90])
-<img src="https://picsum.photos/seed/{{ random_int(0, 10000) }}/{{ $width }}" alt="" class="rounded-xl">
+@props(['employer', 'width' => 90])
+<img src="{{ $employer->logo }}" alt="" class="rounded-xl" width="{{ $width }}">
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -15,6 +15,6 @@
             @endforeach
         </div>
 
-        <x-employer-logo :width="42"/>
+        <x-employer-logo :employer="$job->employer"/>
     </div>
 </x-panel>
